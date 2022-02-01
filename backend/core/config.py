9efe69bfe22884438b6fa54be5c05f2834e4bd01
Tal <kabina.general@gmail.com>
2022-02-01 22:20:20 +0200

    neccesary commit

---
+++ backend/core/config.py
@@ -0,0 +1,23 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+
+class Settings:
+    PROJECT_TITLE: str = "Mimic - Social Media"
+    PROJECT_VERSION: str = "0.0.1"
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
+    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "db_mimic")
+    DATABASE_URL: str = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30
+    ALGORITHM = "HS256"
+
+
+settings = Settings()
